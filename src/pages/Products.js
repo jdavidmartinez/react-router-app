534539a7356c9237fa036a949e3495b32997c80e
DaviD D <jdavid.martinez@gmail.com>
2021-07-20 20:47:12 -0500

    Section 20 - Video 272 - Using 'Switch' and 'exact' for configurong routes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
 const Products = () => {
   return (
     <section>
       <h1>The Products Page</h1>
       <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-
+        <li>
+          <Link to="/products/p1">Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">Camera</Link>
+        </li>
       </ul>
     </section>
   );
